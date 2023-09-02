a1241fd2ac4222c150a0b6f9d919756be2e694ec
Eryk Michalak <gnu.ewm@protonmail.com>
2023-09-02 23:13:26 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: Pineapple Tracker Player/Application
    Translate-URL: https://translate.codeberg.org/projects/pineapple-tracker-player/application/pl/

--- quick/translations/pineapple-tracker-player_pl.ts
+++ quick/translations/pineapple-tracker-player_pl.ts
@@ -7,71 +7,71 @@
         <location filename="../main.qml" line="18"/>
         <location/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Plik</translation>
     </message>
     <message>
         <location filename="../main.qml" line="21"/>
         <location/>
         <source>&amp;Open</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Otwórz</translation>
     </message>
     <message>
         <location filename="../main.qml" line="30"/>
         <location/>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Ustawienia</translation>
     </message>
     <message>
         <location filename="../main.qml" line="33"/>
         <location/>
         <source>Set Mono &amp;Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Ustaw czcionkę o stałej szerokości</translation>
     </message>
     <message>
         <location filename="../main.qml" line="40"/>
         <location/>
         <source>Toggle &amp;Menu Bar Visibility</source>
-        <translation type="unfinished"></translation>
+        <translation>Przełącz widoczność &amp;menu</translation>
     </message>
     <message>
         <location filename="../main.qml" line="66"/>
         <location/>
         <source>Song Title (Drop file to load/play)</source>
-        <translation type="unfinished"></translation>
+        <translation>Tytuł piosenki (Upuść plik aby załadować/odtworzyć)</translation>
     </message>
     <message>
         <location filename="../main.qml" line="70"/>
         <location/>
         <source>Artist (Tracker)</source>
-        <translation type="unfinished"></translation>
+        <translation>Artysta</translation>
     </message>
     <message>
         <location filename="../main.qml" line="84"/>
         <location/>
         <source>Pause</source>
-        <translation type="unfinished"></translation>
+        <translation>Zatrzymaj</translation>
     </message>
     <message>
         <location filename="../main.qml" line="84"/>
         <location/>
         <source>Play</source>
-        <translation type="unfinished"></translation>
+        <translation>Odtwarzaj</translation>
     </message>
     <message>
         <location filename="../main.qml" line="115"/>
         <source>Playback Status</source>
-        <translation type="unfinished"></translation>
+        <translation>Status odtwarzania</translation>
     </message>
     <message>
         <location filename="../main.qml" line="224"/>
         <location/>
         <source>Select module file</source>
-        <translation type="unfinished"></translation>
+        <translation>Wybierz plik modułu</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="24"/>
         <source>File list.</source>
-        <translation type="unfinished"></translation>
+        <translation>Lista plików.</translation>
     </message>
 </context>
 </TS>
