81e921e64eb43beaa5de9af182ea4fd2a6284d90
Oğuz Ersen <oguz@ersen.moe>
2024-01-28 16:24:29 +0000

    Added translation using Weblate (Turkish)

---
+++ quick/translations/pineapple-tracker-player_tr.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="18"/>
+        <location/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="21"/>
+        <location/>
+        <source>&amp;Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="30"/>
+        <location/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="33"/>
+        <location/>
+        <source>Set Mono &amp;Font</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="40"/>
+        <location/>
+        <source>Toggle &amp;Menu Bar Visibility</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="66"/>
+        <location/>
+        <source>Song Title (Drop file to load/play)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="70"/>
+        <location/>
+        <source>Artist (Tracker)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="84"/>
+        <location/>
+        <source>Pause</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="84"/>
+        <location/>
+        <source>Play</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="115"/>
+        <source>Playback Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="224"/>
+        <location/>
+        <source>Select module file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="24"/>
+        <source>File list.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
